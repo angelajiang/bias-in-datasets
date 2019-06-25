1631003f41e6af1e58a649421fa236a7d0306e0a
Angela Jiang <to.ajiang@gmail.com>
2019-06-25 13:10:31 -0400

    Update predict_threshold_simulator and pickles

--- active_learning/src/memoization/predict_threshold_simulator.py
+++ active_learning/src/memoization/predict_threshold_simulator.py
@@ -3,36 +3,56 @@ import numpy as np
 import pickle
 import pprint as pp
 import os
+from pathlib2 import Path
 from sklearn import gaussian_process
 from sklearn.gaussian_process.kernels import Matern, WhiteKernel, ConstantKernel, RBF
 
+def predict_selection(coin_flip, y_pred, std):
+    if (y_pred - std) > coin_flip:
+        prediction = 1
+    else:
+        prediction = 0
+    #print("Y={} std={}, coin flip={}: Predicted={}".format(y_pred, std, coin_flip, prediction))
+    return prediction
+
+def load_data(pickle_file):
+    parsed_pickle_file = "pickles/parsed_{}".format(os.path.basename(pickle_file))
+    ppf = Path(parsed_pickle_file)
+    if ppf.is_file():
+        print("Loading {}...".format(parsed_pickle_file))
+        with open(parsed_pickle_file, 'rb') as handle:
+            vals_by_image_id = pickle.load(handle)
+    else:
+        print("Loading {}...".format(pickle_file))
+        vals_by_image_id = {}
+        with open(pickle_file, 'rb') as handle:
+            d = pickle.load(handle)
+            for image_id, ys in d.iteritems():
+                vals_by_image_id[image_id] = ys
+        with open(parsed_pickle_file, 'wb') as handle:
+            pickle.dump(vals_by_image_id, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    return vals_by_image_id
 
 def predict_iterative(pickle_prefix, fout):
 
     kernel = ConstantKernel() + Matern(length_scale=2, nu=3/2) + WhiteKernel(noise_level=1)
-
-    pickle_file = pickle_prefix + "probabilities.pickle"
     gp = gaussian_process.GaussianProcessRegressor(kernel=kernel)
+
+    probabilities_pickle_file = pickle_prefix + "probabilities.pickle"
     selects_pickle_file = pickle_prefix + "selects.pickle"
+    ys_by_image_id = load_data(probabilities_pickle_file)
+    selects_by_image_id = load_data(selects_pickle_file)
 
-    ys_by_image_id = {}
-    selects_by_image_id = {}
-
-    print("Loading probabilities...")
-    with open(pickle_file, 'rb') as handle:
-        d = pickle.load(handle)
-        for image_id, ys in d.iteritems():
-            ys_by_image_id[image_id] = ys
-
-    print("Loading selects...")
-    with open(selects_pickle_file, 'rb') as handle:
-        d = pickle.load(handle)
-        for image_id, selects in d.iteritems():
-            selects_by_image_id[image_id] = selects
-
-    max_ids = 10
+    max_ids = 5
     num_initial_training = 200
     mses = []
+
+    num_predictions = 0.
+    num_correct = 0.
+    num_positives = 0.
+    num_negatives = 0.
+    false_positives = 0.
+    false_negatives = 0.
 
     for i, (image_id, ys) in enumerate(ys_by_image_id.iteritems()):
         if i > max_ids:
@@ -47,20 +67,54 @@ def predict_iterative(pickle_prefix, fout):
                                                                                 len(ys_train),
                                                                                 round(np.mean(ys_train), 4)))
         for i in range(num_initial_training+1, len(ys)):
+            is_selected_actual = 1 - selects[i]
+            y_actual = ys[i]
             xs_test = np.array([i]).reshape(-1,1)
+            coin_flip = np.random.uniform(0, 1)
+            if coin_flip < y_actual:
+                is_selected_hypothetical = 1
+            else:
+                is_selected_hypothetical = 0
+
             y_pred, std = gp.predict(xs_test, return_std=True)
-            print(std)
-            is_selected = 1 - selects[i]
-            fout.write("prediction,{},{},{},{}\n".format(image_id, (xs_test[0]).tolist()[0], y_pred[0], ys[i]))
+            is_selected_pred = predict_selection(coin_flip, y_pred, std)
+
+            fout.write("prediction,{},{},{},{},{},{},{}\n".format(image_id, (xs_test[0]).tolist()[0], y_pred[0], y_actual, is_selected_actual, is_selected_hypothetical, is_selected_pred))
             fout.flush()
+
+            # Calculate accuracy of regression
             mse = get_mse(y_pred, [ys[i]])
             mses.append(mse)
             #print("MSE: {0:5f}".format(mse))
 
+            # Calculate accuracy of selection
+            num_predictions += 1
+            if is_selected_pred == is_selected_actual:
+                num_correct += 0
+            if is_selected_actual:
+                num_positives += 1
+                if not is_selected_pred:
+                    false_negatives += 1
+            else:
+                num_negatives += 1
+                if not is_selected_pred:
+                    false_positives += 1
+
             ys_train = ys[:i]
             X = np.array(range(len(ys_train))).reshape(-1, 1)
             gp.fit(X, ys_train)
-    return np.average(mses)
+
+        accuracy = num_correct / num_predictions
+        false_positive_rate = false_positives /  num_negatives
+        false_negative_rate = false_negatives /  num_positives
+        print("Intermediate Acc:{}, FPR:{}, FNR:{}".format(accuracy,
+                                                           false_positive_rate,
+                                                           false_negative_rate))
+
+    accuracy = num_correct / num_predictions
+    false_positive_rate = false_positives /  num_negatives
+    false_negative_rate = false_negatives /  num_positives
+    return accuracy, false_positive_rate, false_negative_rate
 
 def get_mse(guesses, answers):
     squares = [math.pow(g - a, 2) for g, a in zip(guesses, answers)]
@@ -86,7 +140,9 @@ if __name__ == "__main__":
         output_file = "{}/{}".format(output_subdir, exp)
         with open(output_file, "w+") as fout:
             pickle_prefix = template.format(exp)
-            average_mse = predict_iterative(pickle_prefix, fout=fout)
-            fout.write("average_MSE,{}\n".format(average_mse))
+            accuracy, false_positive_rate, false_negative_rate = predict_iterative(pickle_prefix, fout=fout)
+            fout.write("accuracy,{},fpr,{},fnr,{}\n".format(accuracy,
+                                                                                            false_positive_rate,
+                                                                                            false_negative_rate))
 
     #predict(pickle_prefix, False)
